b5ed74e3bf314158d9feb338c115862dd8a6780f
hyunjae <stevysung@gmail.com>
2022-02-05 12:57:04 +0900

    config 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,30 @@
+const title = `Stevy's wavyLog`
+const description = `Stevy의 개발 블로그 입니다.`
+const thumbnail = "/thumbnail.png"
+const siteUrl = `https://www.stevy.dev`;
+
 module.exports = {
   siteMetadata: {
-    title: `Stevy's wavyLog`,
-    siteUrl: `https://www.stevy.dev`,
-    description: `Stevy의 개발 블로그 입니다.`,
+    title: title,
+    titleTemplate: "%s | Stevy's wavyLog",
+    siteUrl: siteUrl,
+    description: description,
+    author: "hyunjaesung",
+    image: thumbnail
   },
   plugins: [
     `gatsby-plugin-sass`,
     'gatsby-plugin-image',
-    'gatsby-plugin-sitemap',
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: 'gatsby-plugin-manifest', // PWA
       options: {
-        icon: 'src/images/icon.png',
+        name: `Stevy's wavyLog`,
+        short_name: 'wavyLog',
+        start_url: '/',
+        background_color: `#2e3437`,
+        theme_color: `#2e3437`,
+        display: 'standalone',
+        icon: 'src/images/profile.jpeg',
       },
     },
     'gatsby-plugin-sharp',
@@ -40,7 +53,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-page-creator',
       options: {
-        path: `${__dirname}/src/posts`,
+        path: `${__dirname}/src/posts/`,
       },
     },
     {
@@ -48,7 +61,21 @@ module.exports = {
       extensions: ['mdx', 'md'],
       options: {
         gatsbyRemarkPlugins: [
-          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null, // `~`에 대한 오지랖
+              aliases: {
+                js: "javascript",
+                ts: "typescript",
+                py: "python",
+                kt: "kotlin",
+              },
+              showLineNumbers: true,
+              noInlineHighlight: true,
+            },
+          },
           {
             resolve: `gatsby-remark-images`,
             options: {
@@ -59,6 +86,29 @@ module.exports = {
         ],
       },
     },
+    // SEO
+    {
+      resolve: "gatsby-plugin-next-seo",
+      options: {
+        language: "ko",
+        openGraph: {
+          title: title,
+          description: description,
+          type: "website",
+          locale: "ko_KR",
+          url: siteUrl,
+          site_name: title,
+          images: [
+            {
+              url: thumbnail,
+              width: 800,
+              height: 400,
+              alt: "stevy's wavy log thumbnail",
+            },
+          ],
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
@@ -66,5 +116,76 @@ module.exports = {
       },
     },
     'gatsby-remark-autolink-headers',
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `
+          {
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+            allMdx {
+              nodes {
+                parent {
+                  ... on File {
+                    modifiedTime
+                  }
+                }
+                slug
+              }
+            }
+          }
+        `,
+
+        resolveSiteUrl: () => siteUrl,
+
+        resolvePages: ({
+          allSitePage: { nodes: allPages },
+          allMdx: { nodes: allBlogPostPages },
+        }) => {
+          console.log(allPages, allBlogPostPages);
+          const wpNodeMap = allBlogPostPages.reduce((acc, node) => {
+            const {
+              parent: { modifiedTime },
+              slug,
+            } = node;
+            const uri = `/${slug}/`;
+            acc[uri] = { modifiedTime };
+
+            return acc;
+          }, {});
+
+          return allPages.map((page) => {
+            console.log(wpNodeMap[page.path]);
+            return { ...page, ...wpNodeMap[page.path] };
+          });
+        },
+
+        excludes: [],
+
+        filterPages: ({ path }, excludedRoute) => {
+          return path.startsWith(excludedRoute);
+        },
+
+        serialize: ({ path, modifiedTime }) => {
+          if (modifiedTime) {
+            return {
+              url: path,
+              lastmod: modifiedTime,
+              changefreq: `monthly`,
+              priority: 0.9,
+            };
+          }
+          return {
+            url: path,
+            lastmod: path === "/" ? new Date() : null,
+            changefreq: path === "/" ? "weekly" : `monthly`,
+            priority: path === "/" ? 0.9 : 0.7,
+          };
+        },
+      },
+    },
   ],
 };
