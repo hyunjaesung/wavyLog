0b8d5445d6d867a0219bb2368b2546f401b4a687
stevy.sung <stevy.sung@kakaocorp.com>
2023-12-10 17:55:58 +0900

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <span>All rights reserved © Stevy Sung, 2022</span>
+      <span>All rights reserved © Stevy Sung, 2023</span>
     </footer>
   );
 };
